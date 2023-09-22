ce96a58d17f153c51db27dc457d5ac181f635ec2
brigman12345 <145796773+brigman12345@users.noreply.github.com>
2023-09-22 12:25:53 -0500

    Create python_program2.py

---
+++ python_program2.py
@@ -0,0 +1,29 @@
+choice1=input("So you need some recommendations; do you want to watch an anime or a movie? ")
+
+if choice1 == "Anime":
+  choice2 = input("What genre? Action, Romance, Slice of Life, Isekai, Comedies, or Fantsy ? ")
+  if choice2 == "Action":
+    print("I recommend these three The God of Highschool, Vinland Saga, and Akame Ga Kill. ")
+  if choice2 == "Romance":
+      print("I would recommend Love is War, The Angel Next door spoils me rotten, and Guilty Crown.")
+  if choice2 == "Slice of Life":
+        print("I like and would recommend these three My Teen Romantic Comedy, My Happy Marriage, More Than a Married Couple, But Not lovers, and Horimiya.")
+  if choice2 == "Isekai":
+          print("This is one of my favortie genres and I would recommend these three That time I got reincranted as a slime, Tsukimichi Moonlit Fantasy, and my fav one The Eminence is Shadow.")
+  if choice2 == "Comedies":
+            print("The ones that I like from this genre and would recommend would be these three; One Punch Man, Mob Phsycho 100, and Noragami.")
+  if choice2 == "Fantsy":
+                print("From the ones i've seen I would recommend these, Bleach, One Piece, and Fairy Tail")
+
+elif choice1 == "Movie":
+  choice3 = input("What genre? Action, Romance, Horror , Thriller , Comedies, or Fantsy.")
+  if choice3 == "Action":
+    print("From the ones I have seen I would recommend these three Takers, The John Wick series, and Fury. ")
+  if choice3 == "Romance":
+    print("From the ones that i've watched I would recommend these three NoHardFeelings, After, and Time is up.")
+  if choice3 == "Horror":
+    print("I don't really watch this genre but I would recommend these three The Nun, Halloween, and Smile.")
+  if choice3 == "Thriller":
+    print("I kinda like this genre and I would recommend these three Unhinged, Watcher, and The Girl who Escaped.")
+  if choice3 == "Comedies":
+    print("")
